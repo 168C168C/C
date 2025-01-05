7f5d83e3083ff090dcd338841f2a915e13625679
168C168C <2841812504@qq.com>
2025-01-05 13:36:26 +0800

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,16 +5,16 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, REACT_APP_PRIVATE_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.26",
-   defaultNetwork: "nft",
+   defaultNetwork: "point",
    networks: {
       hardhat: {},
-      nft: {
+      point: {
          url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`],
+         accounts: [`0x${REACT_APP_PRIVATE_KEY}`],
          gas: 2100000,
          gasPrice: 20000000000,
       }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,15 +6,18 @@ async function main() {
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  // 部署 MyNFT 合约
-  const NFT = await hre.ethers.getContractFactory("MyNFT");
-  const nft = await NFT.deploy();
-  console.log("MyNFT contract deployed to:", nft.address);
+  // 部署 UniversalPoints 合约 (通用积分合约)
+  const UniversalPoints = await hre.ethers.getContractFactory("UniversalPoints");
+  const initialSupply = hre.ethers.utils.parseUnits("1000000", 18); // 发行初始的 1000000 UPT
+  const universalPoints = await UniversalPoints.deploy(initialSupply);
+  console.log("UniversalPoints contract deployed to:", universalPoints.address);
+  console.log("Initial supply of UPT:", hre.ethers.utils.formatUnits(initialSupply, 18)); // 格式化并打印初始供应量
 
-  // 如果有 Market 合约，也可以在这里部署
-  const Market = await hre.ethers.getContractFactory("Market");
-  const market = await Market.deploy(nft.address);
-  console.log("Market contract deployed to:", market.address);
+
+  // 部署 PointsExchange 合约 (积分兑换合约)
+  const PointsExchange = await hre.ethers.getContractFactory("PointsExchange");
+  const pointsExchange = await PointsExchange.deploy(universalPoints.address);  // 将 UPT 合约地址传入
+  console.log("PointsExchange contract deployed to:", pointsExchange.address);
 }
 
 main()
